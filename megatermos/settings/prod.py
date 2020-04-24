4bc4a0b36f7260f557ffe70d759b645fcdf73c06
mukhtar <dagdahzub@mail.ru>
2020-04-24 03:53:38 +0300

    fix prod static

--- megatermos/settings/prod.py
+++ megatermos/settings/prod.py
@@ -2,3 +2,6 @@ from .base import *
 
 DEBUG = True
 ALLOWED_HOSTS = ['*']
+
+STATIC_ROOT = os.path.join(os.path.expanduser('~'), 'domains/megatermos24.ru/assets/')
+MEDIA_ROOT = os.path.join(os.path.expanduser('~'), 'domains/megatermos24.ru/media/')
